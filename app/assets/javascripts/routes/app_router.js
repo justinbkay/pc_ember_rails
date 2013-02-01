2632cdc299ef57aade2b5bf44dbae97bfb1c767c
Justin B Kay <jkay@nwrecc.org>
2013-02-01 15:57:18 -0700

    Adding the setup controller for the menu, food controller model

--- app/assets/javascripts/routes/app_router.js
+++ app/assets/javascripts/routes/app_router.js
@@ -4,6 +4,12 @@ App.Router.map(function() {
   });
 });
 
+App.ApplicationRoute = Ember.Route.extend({
+  setupController: function() {
+    this.controllerFor('food').set('model', App.Food.find());
+  }
+});
+
 App.TablesRoute = Ember.Route.extend({
   model: function() {
     return App.Table.find();
