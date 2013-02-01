a417d96bce081a9fe5d316f7f6c5a54fa0b13cd2
Justin B Kay <jkay@nwrecc.org>
2013-02-01 15:58:33 -0700

    creating the food data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Table.create([{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}])
+Food.create([ {id: 1, name: 'Pizza', image_url: 'assets/pizza.png', cents: 1500},
+              {id: 2, name: 'Pancakes', image_url: 'assets/pancakes.png', cents: 300},
+              {id: 3, name: 'Fries', image_url: 'assets/fries.png', cents: 700},
+              {id: 4, name: 'Hot Dog', image_url: 'assets/hotdog.png', cents: 950},
+              {id: 5, name: 'Birthday Cake', image_url: 'assets/birthdaycake.png', cents: 2000}
+            ])
