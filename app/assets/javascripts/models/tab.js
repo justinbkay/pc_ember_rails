08b2efbad7bbfb526e71e12054b12ecd79046abc
Justin <justinbkay@gmail.com>
2013-02-02 15:26:27 -0700

    add a total for a tab

--- app/assets/javascripts/models/tab.js
+++ app/assets/javascripts/models/tab.js
@@ -1,3 +1,8 @@
 App.Tab = DS.Model.extend({
-  tabItems: DS.hasMany('App.TabItem')
+  tabItems: DS.hasMany('App.TabItem'),
+  cents: function() {
+    return this.get('tabItems').getEach('cents').reduce(function(accum, item) {
+      return accum + item;
+    }, 0);
+  }.property('tabItems.@each.cents')
 });
