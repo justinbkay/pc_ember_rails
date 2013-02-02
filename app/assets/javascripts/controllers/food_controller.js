4a99f1479d82ebb78c04becccc6ed531e9db4af9
Justin <justinbkay@gmail.com>
2013-02-02 15:25:55 -0700

    Adding a food item to a tab

--- app/assets/javascripts/controllers/food_controller.js
+++ app/assets/javascripts/controllers/food_controller.js
@@ -1,1 +1,10 @@
-App.FoodController = Ember.ArrayController.extend();
+App.FoodController = Ember.ArrayController.extend({
+  addFood: function(food) {
+    var table = this.controllerFor('table').get('model'),
+        tabItems = table.get('tab.tabItems');
+    tabItems.createRecord({
+      food: food,
+      cents: food.get('cents')
+    })
+  }
+});
